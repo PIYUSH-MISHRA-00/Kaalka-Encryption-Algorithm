42c4a68ac10e7f390f8e3e11d08acb1cb1a69c44
PIYUSH MISHRA <piyushmishra.professional@gmail.com>
2025-07-22 14:53:17 +0530

    File handling functionlity

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='kaalka',
-    version='2.0',
+    version='3.0',
     packages=find_packages(),
     install_requires=[
         'ntplib'
